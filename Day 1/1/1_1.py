c0e3c5066f3055b79565afa9649aac3774e99f75
David Bradburn <42315620+david-bradburn@users.noreply.github.com>
2022-12-01 09:27:59 +0000

    Day 1

--- Day 1/1/1_1.py
+++ Day 1/1/1_1.py
@@ -2,7 +2,7 @@
 ###### https://adventofcode.com/2022/day/1 ######################
 #################################################################
 
-file = "test.txt"
+file = "input.txt"
 
 DAY_NO = "1"
 PART = "1"
@@ -10,4 +10,21 @@ PART = "1"
 file_path_base = "Day " + DAY_NO + "/misc/"
 
 with open(file_path_base + file, "r") as fd:
-	pass
+	raw_input = fd.readlines()
+
+	raw_arr = []
+	for item in raw_input:
+		raw_arr += [item.strip('\n')]
+
+print(raw_arr)
+max_cal = 0
+temp = 0
+for item in raw_arr:
+	if item != '':
+		temp += int(item)
+	else:
+		if temp > max_cal:
+			max_cal = temp
+		
+		temp = 0
+print(max_cal)
--- Day 1/2/1_2.py
+++ Day 1/2/1_2.py
@@ -1,8 +1,8 @@
 #################################################################
-###### https://adventofcode.com/2022/day/1 #######################
+###### https://adventofcode.com/2022/day/1 ######################
 #################################################################
 
-file = "test.txt"
+file = "input.txt"
 
 DAY_NO = "1"
 PART = "2"
@@ -10,4 +10,29 @@ PART = "2"
 file_path_base = "Day " + DAY_NO + "/misc/"
 
 with open(file_path_base + file, "r") as fd:
-	pass
+	raw_input = fd.readlines()
+
+	raw_arr = []
+	for item in raw_input:
+		raw_arr += [item.strip('\n')]
+
+# print(raw_arr)
+total_cal_arr = []
+temp = 0
+
+for item in raw_arr:
+	if item != '':
+		temp += int(item)
+	else:
+		total_cal_arr += [temp]
+		temp = 0
+
+total_cal_arr += [temp]
+total_cal_arr.sort()
+print(total_cal_arr)
+
+total_cal = 0
+for i in range(3):
+	total_cal += total_cal_arr[-1-i]
+
+print(total_cal)
