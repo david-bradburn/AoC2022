849966738f8a05f2fb0718a915dbf76b96c88091
David Bradburn <42315620+david-bradburn@users.noreply.github.com>
2022-12-01 10:12:30 +0000

    Day 2 Creation

---
+++ Day 2/1/2_1.py
@@ -0,0 +1,13 @@
+#################################################################
+###### https://adventofcode.com/2022/day/2 #######################
+#################################################################
+
+file = "test.txt"
+
+DAY_NO = "2"
+PART = "1"
+
+file_path_base = "Day " + DAY_NO + "/misc/"
+
+with open(file_path_base + file, "r") as fd:
+	pass
