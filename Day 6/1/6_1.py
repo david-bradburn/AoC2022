b28c18aad99baffc9909c6d4c4ec6eb3602eeee4
David Bradburn <42315620+david-bradburn@users.noreply.github.com>
2022-12-06 14:00:37 +0000

    Day 6
    Required a little rethink as to the solution for p1 but happy with the result
    
    very quick change for p2

--- Day 6/1/6_1.py
+++ Day 6/1/6_1.py
@@ -2,7 +2,7 @@
 ###### https://adventofcode.com/2022/day/6 ######################
 #################################################################
 
-file = "test1.txt"
+file = "input.txt"
 
 DAY_NO = "6"
 PART = "1"
@@ -20,30 +20,65 @@ class Radio():
 		self.packets = input
 		self.buffer = ''
 
-		self.checkForRepeats()
+		self.main()
 
 	
-	def checkForRepeats(self):
+	def main(self):
 		for packet_index, packet in enumerate(self.packets):
-			if len(self.buffer) == 3:
-				if packet not in self.buffer:
-					print(packet_index)
-					self.buffer += packet
+			if len(self.buffer) < 4:
+				self.buffer += packet
+			else: 
+				self.shiftWindowOnBuffer(packet)
+			
+			if len(self.buffer) == 4:
+				if not self.checkForRepeats():
+					print(packet_index+1)
 					print(self.buffer)
 					break
-				
-				else:
-					self.shiftWindowOnBuffer(packet)
+					
+			
+			
+			
 
-			else:
-				print(self.buffer)
-				assert len(self.buffer) < 3
-				self.buffer += packet
+		
+		# for packet_index, packet in enumerate(self.packets):
+		# 	if len(self.buffer) == 3:
+		# 		if packet not in self.buffer:
+		# 			print(packet_index)
+		# 			self.buffer += packet
+		# 			print(self.buffer)
+		# 			break
+				
+		# 		else:
+		# 			self.shiftWindowOnBuffer(packet)
+
+		# 	else:
+		# 		print(self.buffer)
+		# 		assert len(self.buffer) < 3
+		# 		self.buffer += packet
+
+	def checkForRepeats(self):
+		
+		repeatsCount = 0
+		for character_index, character in enumerate(self.buffer):
+			temp = self.buffer
+			temp = temp.replace(character, '', 1)
+			# print(self.buffer)
+			# print(temp)
+			if character in temp:
+				repeatsCount += 1
+		
+		if repeatsCount > 0:
+			return True
+		else:
+			return False
+
+
 
 	def shiftWindowOnBuffer(self, packet):
 		self.buffer += packet
 		self.buffer = self.buffer[1:]
-		assert len(self.buffer) == 3
+		assert len(self.buffer) == 4
 
 
 p1 = Radio(cleanedInput)
--- Day 6/2/6_2.py
+++ Day 6/2/6_2.py
@@ -1,8 +1,8 @@
 #################################################################
-###### https://adventofcode.com/2022/day/6 #######################
+###### https://adventofcode.com/2022/day/6 ######################
 #################################################################
 
-file = "test.txt"
+file = "input.txt"
 
 DAY_NO = "6"
 PART = "2"
@@ -10,4 +10,53 @@ PART = "2"
 file_path_base = "Day " + DAY_NO + "/misc/"
 
 with open(file_path_base + file, "r") as fd:
-	pass
+	rawInput = fd.readlines()[0]
+
+cleanedInput = rawInput.strip('\n')
+
+class Radio():
+
+	def __init__(self, input) -> None:
+		self.packets = input
+		self.buffer = ''
+
+		self.main()
+
+	
+	def main(self):
+		for packet_index, packet in enumerate(self.packets):
+			if len(self.buffer) < 14:
+				self.buffer += packet
+			else: 
+				self.shiftWindowOnBuffer(packet)
+			
+			if len(self.buffer) == 14:
+				if not self.checkForRepeats():
+					print(packet_index+1)
+					print(self.buffer)
+					break
+					
+	def checkForRepeats(self):
+		
+		repeatsCount = 0
+		for character_index, character in enumerate(self.buffer):
+			temp = self.buffer
+			temp = temp.replace(character, '', 1)
+
+			if character in temp:
+				repeatsCount += 1
+		
+		if repeatsCount > 0:
+			return True
+		else:
+			return False
+
+
+
+	def shiftWindowOnBuffer(self, packet):
+		self.buffer += packet
+		self.buffer = self.buffer[1:]
+		assert len(self.buffer) == 14
+
+
+p1 = Radio(cleanedInput)
