3a68c4a9e0012444f79a8fc4e3630db92a6721af
David Bradburn <42315620+david-bradburn@users.noreply.github.com>
2022-12-07 16:56:25 +0000

    Day 8 Creation

---
+++ Day 8/2/8_2.py
@@ -0,0 +1,13 @@
+#################################################################
+###### https://adventofcode.com/2022/day/8 #######################
+#################################################################
+
+file = "test.txt"
+
+DAY_NO = "8"
+PART = "2"
+
+file_path_base = "Day " + DAY_NO + "/misc/"
+
+with open(file_path_base + file, "r") as fd:
+	pass
