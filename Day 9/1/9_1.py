06493c4861579de91e8c07067f1d15b88519b33f
David Bradburn <42315620+david-bradburn@users.noreply.github.com>
2022-12-08 10:52:15 +0000

    Day 9 file creation

---
+++ Day 9/1/9_1.py
@@ -0,0 +1,13 @@
+#################################################################
+###### https://adventofcode.com/2022/day/9 #######################
+#################################################################
+
+file = "test.txt"
+
+DAY_NO = "9"
+PART = "1"
+
+file_path_base = "Day " + DAY_NO + "/misc/"
+
+with open(file_path_base + file, "r") as fd:
+	pass
