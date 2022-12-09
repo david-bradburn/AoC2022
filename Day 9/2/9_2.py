d9d1db6a52152529e0decccb57191f7ceaa965d6
David Bradburn <42315620+david-bradburn@users.noreply.github.com>
2022-12-09 13:03:27 +0000

    Day 9
    Spent a lot of time searching for invalid physics diagonals got me

--- Day 9/2/9_2.py
+++ Day 9/2/9_2.py
@@ -2,7 +2,7 @@
 ###### https://adventofcode.com/2022/day/9 ######################
 #################################################################
 
-file = "test1.txt"
+file = "input.txt"
 
 DAY_NO = "9"
 PART = "2"
@@ -20,32 +20,57 @@ for instruction in rawInput:
 	cleanerInput.append(temp)
 # print(cleanerInput)
 
-class Rope():
+class RopeSegment():
 
 	def __init__(self) -> None:
 		self.x = 0
 		self.y = 0
-	
-	def returnPositionNthSegmentTuple(self, segmentN):
-		return (self.x, self.y)
+		self.oldX = 0
+		self.oldY = 0
 
-# class Tail():
-
-# 	def __init__(self) -> None:
-# 		self.x = 0
-# 		self.y = 0
+	def updateSegmentPosition(self, dx, dy):
+		self.oldX = self.x
+		self.oldY = self.y
+		self.x += dx
+		self.y += dy
+		# prui
 	
-# 	def returnPositionTuple(self):
-# 		return (self.x, self.y)
+	def returnSegmentOldPosition(self):
+		return (self.oldX, self.oldY)
+	
+	def returnOldNewPositionDelta(self):
+		oldDx = self.x - self.oldX
+		oldDy = self.y - self.oldY
+		return  oldDx, oldDy
+
+
+class Rope():
+
+	def __init__(self, noSegments) -> None:
+		self.ropeSegments = []
+
+		for i in range(noSegments):
+			self.ropeSegments.append(RopeSegment())
+
+	def returnPositionNthSegmentTuple(self, segmentN: int):
+		segment = self.ropeSegments[segmentN]
+		return (segment.x, segment.y)
+	
+	def updateRopeSegmentPosition(self, ropeSegment, dx, dy):
+		self.ropeSegments[ropeSegment].updateSegmentPosition(dx, dy)
+	
+	# def setTolocation(self, ropeSegment, x, y)
+
 
 class Bridge():
 
 	def __init__(self, input) -> None:
 		self.headInstructions = input
 		
-		self.rope = Rope()
+		self.noSegments = 10
+		self.rope = Rope(self.noSegments)
 		
-		self.tailPositionDict = {self.tail.returnPositionTuple(): 1}
+		self.tailPositionDict = {self.rope.returnPositionNthSegmentTuple(self.noSegments-1): 1}
 		
 		print(self.tailPositionDict)
 		
@@ -53,53 +78,92 @@ class Bridge():
 		self.main()
 	
 	def moveHead(self, instruction):
-		self.oldHeadPosition = (self.head.x, self.head.y)
+		# self.oldHeadPosition = self.rope.returnPositionNthSegmentTuple(0) ##head position
 		match instruction[0]:
 			case 'R':
-				self.head.x += 1
+				self.rope.updateRopeSegmentPosition(0, 1, 0)
 			
 			case 'D':
-				self.head.y += 1
-			
+				self.rope.updateRopeSegmentPosition(0, 0, -1)
+		
 			case 'L':
-				self.head.x -= 1
+				self.rope.updateRopeSegmentPosition(0, -1, 0)
 			
 			case 'U':
-				self.head.y -= 1
+				self.rope.updateRopeSegmentPosition(0, 0, 1)
 
 
-	def checkDistanceOfHeadFromTail(self):
-		dX = self.head.x - self.tail.x ## positive is right
-		dY = self.head.y - self.tail.y ## positice is down
+	def checkDistanceOfAheadSegment(self, segmentNo):
+		dX = self.rope.ropeSegments[segmentNo-1].x - self.rope.ropeSegments[segmentNo].x ## positive is right
+		dY = self.rope.ropeSegments[segmentNo-1].y - self.rope.ropeSegments[segmentNo].y ## positive is up
 
 		return dX, dY
 	
 	def updateTailDict(self):
-		tailPos = self.tail.returnPositionTuple()
+		tailPos = self.rope.returnPositionNthSegmentTuple(self.noSegments - 1) 
 		if tailPos not in self.tailPositionDict:
 			self.tailPositionDict[tailPos] = 1
 		else:
 			self.tailPositionDict[tailPos] += 1
 
-	def updateTailPosition(self):
+	def updateSegmentPosition(self, segmentNo):
 		#move to where the head was unless too close
-		dX, dY = self.checkDistanceOfHeadFromTail()
-		if abs(dX) <= 1 and abs(dY) <= 1:
-			pass
+		dX, dY = self.checkDistanceOfAheadSegment(segmentNo)
+		oldDx, oldDy = self.rope.ropeSegments[segmentNo - 1].returnOldNewPositionDelta()
 
-		else: ##move to old head position
-			self.tail.x = self.oldHeadPosition[0]
-			self.tail.y = self.oldHeadPosition[1]
-			self.updateTailDict()
+		oldx, oldy = self.rope.ropeSegments[segmentNo-1].returnSegmentOldPosition()
+		# curX, curY = self.rope.returnPositionNthSegmentTuple(segmentNo)
+		# oldPositionDeltaX = oldx - curX
+		# oldPositionDeltaY = oldy - curY
+
+
+		if abs(dX) <= 1 and abs(dY) <= 1: ## still in range
+			self.rope.updateRopeSegmentPosition(segmentNo, 0, 0)
+		
+		elif abs(oldDx) == 1 and abs(oldDy) == 1: ## check for diagonal motion
+			if abs(dX) > 0 and abs(dY) > 0:
+				self.rope.updateRopeSegmentPosition(segmentNo, oldDx, oldDy)
+			else:
+				self.rope.updateRopeSegmentPosition(segmentNo, int(dX/2), int(dY/2))
+
+		# elif abs(oldDx) == 1 and abs(oldDy) == 1:
+		# 	self.rope.updateRopeSegmentPosition(segmentNo, int(dX/2), int(dY/2))
+
+		else: ##move to old segment position
+			oldx, oldy = self.rope.ropeSegments[segmentNo-1].returnSegmentOldPosition()
+			x, y = self.rope.returnPositionNthSegmentTuple(segmentNo)
+			# print(oldx, oldy, x, y)
+			
+			self.rope.updateRopeSegmentPosition(segmentNo, oldx - x, oldy - y)
+			# 
+
+	def display(self):
+		for i in range(self.noSegments):
+			print("{} {}".format(i ,self.rope.returnPositionNthSegmentTuple(i)))
 
 	def main(self):
+		self.display()
 		for instruction in self.headInstructions:
 			direction = instruction[0]
 			distance = instruction[1]
+			
+			print('---------{}:{}---------'.format(direction, distance))
 			for amount in range(distance):
+				
 				self.moveHead(direction)
-				self.updateTailPosition()
-
-		print(len(self.tailPositionDict))	
+				# print(amount)
+				# print('===========')
+				
+				for segmentNo in range(1, self.noSegments):
+					# print(segmentNo)
+					self.updateSegmentPosition(segmentNo)
+				
+				if direction == 'L' and distance == 8:
+					print('--------')
+					self.display()
+				
+				self.updateTailDict()
+			self.display()
+		print(len(self.tailPositionDict))
 
 p1 = Bridge(cleanerInput)
