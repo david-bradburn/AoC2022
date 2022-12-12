36e76e86edc67fb745dacf9abcace39702989354
David Bradburn <42315620+david-bradburn@users.noreply.github.com>
2022-12-12 14:15:57 +0000

    Day 10 p2
    Very fun although the counter print bug wasn't fun to debug :(

--- Day 10/2/10_2.py
+++ Day 10/2/10_2.py
@@ -4,7 +4,7 @@
 
 import numpy as np
 
-file = "test.txt"
+file = "input.txt"
 
 DAY_NO = "10"
 PART = "2"
@@ -21,7 +21,7 @@ class ComSystem():
 
 	def __init__(self, input) -> None:
 		self.input = input
-		self.output = []
+		# self.output = []
 		self.regValue = 1
 		self.clockCycleCounter = 0
 
@@ -29,7 +29,7 @@ class ComSystem():
 
 		
 
-		self.cyclesToPrint = [19,59,99,139,179,219]
+		# self.cyclesToPrint = [19,59,99,139,179,219]
 		self.display = []
 
 		self.main()
@@ -40,29 +40,19 @@ class ComSystem():
 	def incrementClockCount(self):
 		self.clockCycleCounter += 1
 
-	def addToOutput(self):
-		self.output.append([self.clockCycleCounter, self.regValue])
-	
-
-	# def printOutput(self):
-	# 	temp = 0
-	# 	for line in self.output:
-			
-	# 		if line[0] in self.cyclesToPrint:
-	# 			temp += (line[0]+ 1) * line[1]
-	# 			print(line)
-	# 	print(temp)
 
 	def updateDisplay(self):
 		pongPosition = [self.regValue - 1, self.regValue, self.regValue + 1]
-		
-		if self.clockCycleCounter in pongPosition:
+		modClockCount = self.clockCycleCounter % 40
+		if modClockCount in pongPosition:
 			self.display.append('#')
 		else:
 			self.display.append('.')
 	
 	def displayScreen(self):
-		output = np.reshape(self.display, (40, 6)).T
+		assert len(self.display) == 240
+		# print(self.display)
+		output = np.reshape(self.display, (6, 40))
 		# lengthDisplay = len(self.display)
 		for row in output:
 			temp = ''
@@ -93,7 +83,7 @@ class ComSystem():
 				self.updateDisplay()
 				##
 				self.incrementClockCount()
-				self.addToOutput()
+				# self.addToOutput()
 			
 			case 'addx':
 				for instructionCycle in range(self.addxTime):
@@ -104,7 +94,7 @@ class ComSystem():
 					self.incrementClockCount()
 					if instructionCycle == 1:
 						self.updateRegister(value)
-					self.addToOutput()
+					# self.addToOutput()
 		
 
 
