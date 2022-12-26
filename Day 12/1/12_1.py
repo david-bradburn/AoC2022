fa38f36b73b4f0ef345c98050c8738c6cb5bba7b
David Bradburn <42315620+david-bradburn@users.noreply.github.com>
2022-12-26 00:25:12 +0000

    Day 12 p1 update

--- Day 12/1/12_1.py
+++ Day 12/1/12_1.py
@@ -2,6 +2,8 @@
 ###### https://adventofcode.com/2022/day/12 #####################
 #################################################################
 
+import numpy as np
+
 file = "test.txt"
 
 DAY_NO = "12"
@@ -11,3 +13,41 @@ file_path_base = "Day " + DAY_NO + "/misc/"
 
 with open(file_path_base + file, "r") as fd:
 	rawInput = fd.readlines()
+
+
+
+class Map():
+
+	def __init__(self, rawInput) -> None:
+		self.rawInput = rawInput
+		
+		self.processInput()
+		# self.display()
+
+	def processInput(self):
+		tempInput = []
+		for row in self.rawInput:
+			tempInput += [row.strip()]
+
+		self.height = len(tempInput)
+		self.width = len(tempInput[0])
+		# print(self.height, self.width)
+		self.map = np.zeros((self.height, self.width), dtype = int)
+		for rowIndex, row in enumerate(tempInput):
+			for coloumnIndex, value in enumerate(row):
+				if value == 'S':
+					self.map[rowIndex][coloumnIndex] = 0
+				
+				elif value == 'E':
+					self.map[rowIndex][coloumnIndex] = 27
+				
+				else:
+					self.map[rowIndex][coloumnIndex] = ord(value) - 96
+
+		print(self.map)
+		# print(tempInput)
+	
+
+	
+
+p1 = Map(rawInput)
