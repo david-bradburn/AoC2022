49f7a2b6d31c5264ec403e512c9a479c3234b8e8
David Bradburn <42315620+david-bradburn@users.noreply.github.com>
2023-06-09 11:20:44 +0100

    Update 13_1.py

--- Day 13/1/13_1.py
+++ Day 13/1/13_1.py
@@ -18,6 +18,7 @@ stripedInput = [i.strip("\n") for i in rawInput if i != "\n"]
 convertedInput = [ast.literal_eval(i) for i in stripedInput]
 # print(convertedInput)
 
+
 class distress_packets():
 
 	def __init__(self, data) -> None:
@@ -31,6 +32,7 @@ class distress_packets():
 
 	
 	def pair_data(self):
+		"""Converts N in to N/2 lots of 2"""
 		self.paired_data = []
 		assert (len(self.rawData) % 2 == 0)
 		for i in range(0, len(self.rawData), 2): # 
@@ -38,19 +40,53 @@ class distress_packets():
 
 		# print(self.paired_data)
 
-	def check_types(self, left, right):
-		return type(left)
+	def iterate_iter(self, element_iter):
+		try:
+			return next(element_iter)
+		except StopIteration:
+			print("Empty list")
+			return None
 	
 	# def pair_iter(self, left):
 	# 	for left_item in left:
 	
 	def main(self):
 		for pair in self.paired_data:
+			print("------------------------------")
 			left, right = pair
 
-			print(left)
-			for left_item in left:
-				print(left_item, type(left_item))
+			left_iter = iter(left)
+			right_iter = iter(right)
+
+			
+			# print(left)
+
+			left_val = self.iterate_iter(left_iter)
+			right_val = self.iterate_iter(right_iter)
+			print(left_val, right_val)
+			# print(left_iter_type, right_iter_type)
+
+			match (left_val, right_val):
+				
+				case (int(), int()): 
+					print("both int")
+				
+				case (None, _) | (_, None):
+					print("one is empty")
+
+				case (list, int()) | (int(), list):
+					print("list and int")
+
+				case (list, _) | (_, list):
+					print("both list")
+
+				case (_, _):
+					raise Exception
+					print("default")
+
+
+			
+
 
 
 p1 = distress_packets(convertedInput)
