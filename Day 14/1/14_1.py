1c7ac18912cdbe59179355951b1f3eeb993fa4e3
David Bradburn <david.bradburn2@gmail.com>
2023-06-15 14:28:59 +0100

    Day 14 p1 very fun part one, I'm happy with my solution even if it is a little long winded

--- Day 14/1/14_1.py
+++ Day 14/1/14_1.py
@@ -4,7 +4,7 @@
 
 import numpy as np
 
-file = "test.txt"
+file = "input.txt"
 
 DAY_NO = "14"
 PART = "1"
@@ -19,7 +19,7 @@ cleanedInput = [i.strip("\n").split(" -> ") for i in rawInput]
 # print(cleanedInput)
 
 class Cave():
-	
+
 	def __init__(self, wallCoords) -> None:
 		self.rawWallCoords = wallCoords
 		self.minX = 1000
@@ -29,7 +29,12 @@ class Cave():
 		self.maxY = 0
 		self.createWalls()
 		self.sandFinalLoc = []
-		self.displayCave()
+		# self.displayCave()
+
+		self.noSandStopped = 0
+		self.continuousSand = False
+		self.main()
+		print(f"{self.noSandStopped}")
 		pass
 
 
@@ -44,6 +49,50 @@ class Cave():
 				else:
 					temp += "."
 			print(temp)
+
+	def main(self):
+
+		while not self.continuousSand:
+			self.updateSand(500, 0)
+			# self.screen_clear()
+			# self.displayCave()
+		self.displayCave()
+
+	
+	
+	def screen_clear(self):
+
+		LINE_UP = '\033[1A'
+		LINE_CLEAR = '\x1b[2K'
+
+		for i in range(self.maxY + 2):
+			print(LINE_UP, end=LINE_CLEAR)
+
+
+	def updateSand(self, sandCoord_x, sandCoord_y):
+		if sandCoord_y > self.maxY:
+			self.continuousSand = True
+			return
+
+		if not([sandCoord_x, sandCoord_y + 1] in (self.walls + self.sandFinalLoc)):
+			sandCoord_y += 1
+			# self.dropSand(sandCoord_x, sandCoord_y)
+		elif not ([sandCoord_x - 1, sandCoord_y + 1] in (self.walls + self.sandFinalLoc)):
+			sandCoord_x -= 1
+			sandCoord_y += 1
+			
+		elif not ([sandCoord_x + 1, sandCoord_y + 1] in (self.walls + self.sandFinalLoc)):
+			sandCoord_x += 1
+			sandCoord_y += 1
+		else:
+			# print("sand has come to rest")
+			self.sandFinalLoc.append([sandCoord_x, sandCoord_y])
+			self.noSandStopped += 1
+			return
+
+		self.updateSand(sandCoord_x, sandCoord_y)
+		
+			
 
 
 	def createWalls(self):
@@ -64,13 +113,12 @@ class Cave():
 				temp.append((x,y))
 			lessRawWallPoints.append(temp)
 		# print(lessRawWallPoints)
-		print(f"min x {self.minX} min y {self.minY}")
-		print(f"max x {self.maxX} max y {self.maxY}")
-
+		# print(f"min x {self.minX} min y {self.minY}")
+		# print(f"max x {self.maxX} max y {self.maxY}")
 
 		self.walls = []
 		for wall in lessRawWallPoints:
-			print("------------------------")
+			# print("------------------------")
 			for point_index in range(len(wall) - 1):
 				x0 = wall[point_index][0]
 				x1 = wall[point_index+ 1][0]
@@ -78,12 +126,8 @@ class Cave():
 				y0 = wall[point_index][1]
 				y1 = wall[point_index + 1][1]
 
-				# print(x0, y0)
-				# print(x1, y1)
-
-				
 				step = max(abs(x0 - x1), abs(y0 - y1))
-				# print(step)
+				
 				assert(step > 0)
 				temp = np.linspace(wall[point_index], wall[point_index+1], step + 1, dtype=int).tolist()
 
@@ -91,7 +135,7 @@ class Cave():
 					if point not in self.walls:
 						self.walls.append(point)
 
-			print(self.walls)
+			# print(self.walls)
 
 
 cave = Cave(cleanedInput)
