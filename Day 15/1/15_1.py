92ccd90a8457bd12756d634609e4dafbe89ada65
David Bradburn <david.bradburn2@gmail.com>
2023-06-16 10:29:39 +0100

    Day 15 part 1 file creation

---
+++ Day 15/1/15_1.py
@@ -0,0 +1,13 @@
+#################################################################
+###### https://adventofcode.com/2022/day/15 #####################
+#################################################################
+
+file = "test.txt"
+
+DAY_NO = "15"
+PART = "1"
+
+file_path_base = "Day " + DAY_NO + "/misc/"
+
+with open(file_path_base + file, "r") as fd:
+	rawInput = fd.readlines()
