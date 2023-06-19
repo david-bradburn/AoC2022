6754defa9c9be3e3ecae7fd1361323ff377c65c8
David Bradburn <david.bradburn2@gmail.com>
2023-06-19 14:43:03 +0100

    find Mat distance

--- Day 15/1/15_1.py
+++ Day 15/1/15_1.py
@@ -18,8 +18,8 @@ with open(file_path_base + file, "r") as fd:
 	rawInput = fd.readlines()
 
 cleanerInput = [i.strip("\n") for i in rawInput]
-# for i in cleanerInput:
-# 	print(i)
+for i in cleanerInput:
+	print(i)
 
 
 class Scan():
@@ -28,12 +28,26 @@ class Scan():
 		self.data = data
 		self.beaconPositions = []
 		self.sensorPositions = []
+
+		self.sensorDict = {}
+
+
 		self.processData()
+
+		# self.minX = 0
+
+		self.test_y = 10
+		self.p1_y = 2000000
+
+		
 	
 
 	def processData(self):
+		self.minX = 0
+		self.maxX = 0
 		for line in self.data:
 			partialstrip = line.strip("Sensor at x=").split(",")
+
 			xs = int(partialstrip[0])
 			p2 = partialstrip[1].strip(" y=").split(":")
 			ys = int(p2[0])
@@ -41,12 +55,32 @@ class Scan():
 			xb = int(p2[-1].split("=")[-1])
 			yb = int(partialstrip[-1].strip(" y="))
 
+			self.minX = min(self.minX, xb)
+			self.maxX = max(self.maxX, xb)
 			self.sensorPositions.append((xs, ys))
-			if [xb, yb] not in self.beaconPositions:
+			if (xb, yb) not in self.beaconPositions:
 				self.beaconPositions.append((xb, yb))
+			
+			self.sensorDict[(xs, ys)] = self.findMatDist((xs, ys), (xb, yb))
 
-		print(self.sensorPositions)#
+		print(self.sensorPositions)
 		print(self.beaconPositions)
+		print(self.minX, self.maxX)
+
+	def findMatDist(self, sensorCoord, point):
+		xs = sensorCoord[0]
+		ys = sensorCoord[1]
+
+		xp = point[0]
+		yp = point[1]
+
+		return abs(xs - xp) + abs(ys - yp)
+
+	def checkNone(self):
+		self.noneCount = 0
+		for x in range(self.minX, self.maxX + 1):
+			...
+		# ...
 
 	
 	# def 
